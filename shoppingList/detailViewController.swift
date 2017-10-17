ef632de5d7c55375305aace3e9921d6035aabf6e
Trym Lintzen <Trymlintzen@Macintosh-van-Trym.local>
2017-10-17 12:08:55 +0200

    update 6

--- shoppingList/detailViewController.swift
+++ shoppingList/detailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class detailViewController: UIViewController {
+class detailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var itemLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
@@ -34,6 +34,32 @@ class detailViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func takePicture(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+            // if device has camera -> take picture other pick library
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+        } else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // get picked image from info dictionary
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Put that image on the screen in the image view
+        imageViewFood.image = image
+        
+        // take image picker off te screen -
+        // you must call this dismiss method
+        dismiss(animated: true, completion: nil)
     }
     
 
